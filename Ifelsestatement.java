3b5011956aaa19f0daf1a2bbca657455206a3d74
krishnapl123 <krishnapl.xworkz@gmail.com>
2021-10-17 11:19:16 +0530

    writes Ifelsestatement.java

---
+++ Ifelsestatement.java
@@ -0,0 +1,17 @@
+class Ifelsestatement{
+
+public static void main(String args[]){
+
+String name="Krishna";
+
+if(name=="Krishna")
+{
+System.out.println("He is krishna");
+}
+else
+
+System.out.println("He is not  krishna");
+
+
+}
+}
