5975c59498f53dd32ddb762b7887898f9e3160a1
krishnapl123 <krishnapl.xworkz@gmail.com>
2021-10-17 20:32:56 +0530

    writes SingleInheritenceDemo.java

---
+++ SingleInheritenceDemo.java
@@ -0,0 +1,15 @@
+class SingleInheritenceDemo{
+
+public static void main(String args[]){
+
+
+Apple apple=new Apple();
+
+apple.name="Apple";
+apple.color="Red";
+apple.taste="Sweet";
+
+System.out.println( "name "   + apple.name+ " color "  + apple.color+ " taste "  + apple.taste);
+}
+}
+ 
