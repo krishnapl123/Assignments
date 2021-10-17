0dca19e5ec69a344a39ddda0dd2542e626c8af86
krishnapl123 <krishnapl.xworkz@gmail.com>
2021-10-17 11:03:58 +0530

    Forloopdemo.java

---
+++ Forloopdemo.java
@@ -0,0 +1,14 @@
+ class Forloopdemo{
+
+public static void main(String args[])
+{
+   for(int i=0;i<=3;i++)
+     {
+         for(int j=0;j<=3;j++)
+          {
+              System.out.print(j + " ");
+          }
+             System.out.println();
+}
+}
+}
