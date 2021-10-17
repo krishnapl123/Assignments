2371c937e53babc2933d25bad374b1f67dc5cde2
krishnapl123 <krishnapl.xworkz@gmail.com>
2021-10-17 10:38:02 +0530

    writes IfelseifstatementDemo.java

---
+++ IfelseifstatementDemo.java
@@ -0,0 +1,33 @@
+class IfelseifstatementDemo{
+
+public static void main(String args[])
+{
+    boolean isAptitudeRoundclr=true;
+    boolean isTechnicalRoundclr=true;
+    boolean isHRroundclr=true;
+if(isAptitudeRoundclr){
+
+                     if(isTechnicalRoundclr){
+                                              if(isHRroundclr){
+
+System.out.println("congradulations...you are selected");
+}
+else
+{
+System.out.println(" sorry you didn't clear HR round");
+}
+}
+else
+{
+ System.out.println(" sorry you didn't clear Technical round");
+}
+}
+else
+{
+System.out.println(" sorry you didn't clear Aptitude round");
+}
+}
+}
+
+
+
